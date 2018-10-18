45d0787100635e78ff4e4cff0da1f2861bbf3400
baptisteg01@gmail.com <01051997b>
2018-10-18 18:00:28 +0200

    test

--- app/src/main/java/com/example/bgautier/besafe/LoginActivity.java
+++ app/src/main/java/com/example/bgautier/besafe/LoginActivity.java
@@ -82,8 +82,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
         });
-
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.password);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
