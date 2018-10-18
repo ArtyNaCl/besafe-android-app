e10ef8c48509957f852a10b01aca31ee16ac0f3c
baptisteg01@gmail.com <01051997b>
2018-10-18 18:10:56 +0200

    add notifications

--- app/src/main/java/com/example/bgautier/besafe/MainActivity.java
+++ app/src/main/java/com/example/bgautier/besafe/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.bgautier.besafe;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -26,7 +31,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private static final String CHANNEL_ID = "channel_id";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -170,4 +175,36 @@ public class MainActivity extends AppCompatActivity
 // Add the request to the RequestQueue.
         queue.add(stringRequest);
     }
+
+
+    public void sendNotification(String textContent){
+        int  notificationId = 2;
+        // set notification
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Alert de proximité")
+                .setContentText(textContent)
+                .setPriority(NotificationCompat.PRIORITY_MAX);
+        createNotificationChannel();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationId, mBuilder.build());
+    }
+
+    
+    //set notification channel for version <8.0.0
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
