976e6100603508db26b47276c19decbb7b01d695
Hugo Da Roit <contact@hdaroit.fr>
2018-10-18 20:15:14 +0200

    Update geoloc listener

--- app/src/main/java/com/example/bgautier/besafe/GeolocListener.java
+++ app/src/main/java/com/example/bgautier/besafe/GeolocListener.java
@@ -1,10 +1,14 @@
 package com.example.bgautier.besafe;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import com.android.volley.Request;
@@ -14,18 +18,31 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class GeolocListener {
     private final String TAG = "GeolocListener";
-    private LocationManager locationManager;
-    private Context context;
+    private Context activity;
+    private String token;
+    private String userId;
 
-    public GeolocListener(Context context) throws SecurityException {
-        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        this.context = context;
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+    public GeolocListener(Activity activity, String userId, String token) {
+        LocationManager locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+        this.activity = activity;
+        this.userId = userId;
+        this.token = token;
+
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 123);
+        } else {
+            Log.d(TAG, "Already have permissions.");
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, new LocationListener() {
             @Override
             public void onStatusChanged(String provider, int status, Bundle extras) {
                 Log.d(TAG, "Status changed : " + provider + "(" + String.valueOf(status) + ")");
@@ -46,51 +63,50 @@ public class GeolocListener {
             @Override
             public void onLocationChanged(Location location) {
                 Log.d(TAG, "Latitude " + location.getLatitude() + ", longitude " + location.getLongitude());
-                // TODO : Update API
+                callApiLocation(location);
             }
         });
     }
 
-    public Location getLastGeoloc() throws SecurityException {
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-        if (location != null) {
-            return location;
-        }
-
-        return locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-    }
-
-    public void callApiLocation(String id) {
-
-        RequestQueue queue = Volley.newRequestQueue(this.context);
-        String url = "http://hdaroit.fr:3000{}/api/appusers/"+id;
+    private void callApiLocation(final Location location) {
+        RequestQueue queue = Volley.newRequestQueue(activity);
+        String url = "http://hdaroit.fr:3000/api/appusers/" + this.userId + "?access_token=" + this.token;
 
         // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.PATCH, url,
+        StringRequest stringRequest =
+            new StringRequest(Request.Method.PATCH, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        //mTextView.setText("Response is: "+ response.substring(0,500));
+                        Log.i(TAG, response);
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("That didn't work!","");
-            }
-        }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("That didn't work!",error.toString());
+                    }
+                }
         ) {
             @Override
-            protected Map<String, String> getParams()
-            {
-                Map<String, String>  params = new HashMap<String, String>();
-              //  params.put("", );
-                params.put("domain", "http://itsalif.info");
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
 
+                JSONObject loc = new JSONObject();
+
+                try {
+                    loc.put("lat", location.getLatitude());
+                    loc.put("lng", location.getLongitude());
+                } catch (JSONException e) {
+                    Log.e(TAG, e.toString());
+                    return params;
+                }
+
+                params.put("location", loc.toString());
                 return params;
             }
         };
+
         queue.add(stringRequest);
     }
 
